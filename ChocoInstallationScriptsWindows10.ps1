dd730c27738bdea9c95fb8bfed254d7d1266c729
Piotr Kirklewski <azure@vmexpertise.com>
2020-12-04 18:17:47 +0100

    first commit

---
+++ ChocoInstallationScriptsWindows10.ps1
@@ -0,0 +1,55 @@
+﻿choco feature enable -n allowGlobalConfirmation
+
+# DEV
+choco install notepadplusplus -y 
+choco install brackets -y 
+choco install fiddler -y
+choco install wireshark -y 
+choco install nodejs.install -y
+choco install github  -y
+choco install git  -y
+choco install linqpad4  -y
+choco install baretail -y
+choco install ilspy  -y
+choco install dotpeek  -y
+choco install filezilla -y
+choco install putty 
+choco install 7zip  -y
+choco install paint.net -y
+
+# Browsers
+choco install google-chrome-x64 -y
+choco install opera -y
+choco install Firefox -y
+choco install WhatsApp -y
+choco install -y microsoft-teams 
+
+# Optional
+choco install windowsazurepowershell -y 
+choco install rdmfree -y
+choco install keepass -y
+choco install 7zip -y
+choco install agentransack -y
+choco install ccleaner -y
+choco install vscode -y
+choco install vscode-powershell -y
+choco install visualstudio2019community -y
+choco install sql-server-management-studio -y
+choco install putty.install -y
+choco install soapui -y
+choco install postman -y
+choco install treesizefree -y
+choco install diffmerge -y
+choco install guitar-pro -y
+choco install sql-server-2019 -y
+choco install adobe-creative-cloud  -y
+choco install youtube-dl -y
+choco install zoom -y
+choco install -y autodesk-fusion360 
+ 
+  
+ 
+
+
+
+
